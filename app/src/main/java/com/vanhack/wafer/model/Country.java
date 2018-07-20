e1347e5848457d95402614f507ed0a2c238c43fb
William Miranda <wlmirand@gmail.com>
2018-07-20 12:28:37 -0300

    Adding the POJO classes
    Signed-off-by: William Miranda <wlmirand@gmail.com>

---
+++ app/src/main/java/com/vanhack/wafer/model/Country.java
@@ -0,0 +1,38 @@
+package com.vanhack.wafer.model;
+
+/**
+ * POJO for the Country.
+ * In this challenge we have to follow:
+ *
+ * 2. Display json data as listview with following elements parsed from json
+ *    a. “name”  ->  this is Country Name
+ *    b  “currencies” -> ”name" -> this is currency name, if more than 1 currency is present, first currency name is to be displayed
+ *    c. “languages” -> “name”  -> this is language name, if more than 1 language is present first language is to be used
+ *
+ * So we can have one String for each attribute. If we are to display more currencies or languages,
+ * I'd create classes for Currency and Language and add a List of the here
+ */
+public class Country {
+
+    private String mName;
+    private String mCurrency;
+    private String mLanguage;
+
+    public Country(String name, String currency, String language) {
+        this.mName = name;
+        this.mCurrency = currency;
+        this.mLanguage = language;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public String getmCurrency() {
+        return mCurrency;
+    }
+
+    public String getmLanguage() {
+        return mLanguage;
+    }
+}
